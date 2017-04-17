7a4d5567220726faf6493e4c039fbeda7ac909ce
Ibrahim Hussein <i.hussein@posrt.com>
2017-04-17 02:32:24 +0200

    new fix

---
+++ app/src/main/java/com/iaihussein/popularmovies/Var.java
@@ -0,0 +1,17 @@
+package com.iaihussein.popularmovies;
+
+/**
+ * Created by iaihussein on 10/21/16.
+ */
+public class Var {
+    final static String RESULTS = "results", URL_BASE = "https://api.themoviedb.org/3/movie/",
+            KEY = "?api_key=",//the api key
+            REVIEWS = "/reviews",
+            VIDEOS = "/videos",
+            URL_POPULAR = URL_BASE + "popular" + KEY,
+            URL_TOP = URL_BASE + "top_rated" + KEY,
+            URL_IMAGE = "http://image.tmdb.org/t/p/w185",
+            ARG_EXTRA = "movie",
+            URL_YOUTUBE = "http://www.youtube.com/watch?v=";
+
+}
